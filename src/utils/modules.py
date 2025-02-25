1e8271fee436d995d5182809fa8fa4ee4ff52593
MeepOwned13 <sebokmatyas01@gmail.com>
2025-02-25 19:47:41 +0100

    Move utility functions
    Not moved in this commit:
    - create_nerf_data
    - NeRF.compute_along_rays

---
+++ src/utils/modules.py
@@ -0,0 +1,140 @@
+import torch
+from torch import Tensor, nn
+
+
+class PositionalEncoding(nn.Module):
+    """Appended Positional Encoding Module"""
+
+    def __init__(self, max_freq: int):
+        """Init
+        
+        Args:
+            max_freq: Maximum frequency to use for encoding
+        """
+        super(PositionalEncoding, self).__init__()
+        self._max_freq = max_freq
+        """Stores the max frequency"""
+
+        freq_bands = 2.0 ** torch.linspace(0.0, max_freq - 1, steps=max_freq, dtype=torch.float32)
+        self._freq_bands = nn.parameter.Buffer(freq_bands)
+        """Pre-calculated frequency bands for encoding"""
+    
+    def forward(self, x: Tensor) -> Tensor:
+        """Perform Positional Encoding
+
+        Args:
+            x (shape[..., M]): Tensor to encode
+
+        Returns:
+            pe (shape[..., M + M * max_freq * 2]): Tensor with appended Positional Encoding
+        """
+        encs = (x[..., None] * self._freq_bands).flatten(-2, -1)
+        # Encoding to (x, sin parts, cos parts) of shape(N, M+M*max_freq*2) if x is of shape(N,M)
+        return torch.cat([x, encs.sin(), encs.cos()], dim=-1)
+    
+    def get_out_dim(self, in_dim: int):
+        """Returns the outgoing dimension of the forward() method
+
+        Args:
+            in_dim: Last dim of Tensor to be used
+        
+        Returns:
+            out_dim: Last dim after Positional Encoding
+        """
+        return in_dim + in_dim * self._max_freq * 2
+
+
+class NeRF(nn.Module):
+    """NeRF"""
+
+    def __init__(self, num_layers: int = 8, hidden_size: int = 256, in_coordinates: int = 3, in_directions: int = 3,
+                 skips: list[int] = [4], coord_encode_freq: int = 10, dir_encode_freq: int = 4):
+        """Init
+
+        Args:
+            num_layers: Layer count for primary feature MLP
+            hidden_size: Hidden size for all Linear layers
+            in_coordinates: Count of input point coordinates
+            in_directions: Count of input direction coordinates (spherical=>2, cartesian=>3)
+            skips: Skip connection list for primary feature MLP
+            coord_encode_freq: Max frequency for coordinate PE
+            dir_encode_freq: Max frequency for direction PE
+        """
+        super(NeRF, self).__init__()
+        self.in_coordinates = in_coordinates
+        """Count of input point coordinates"""
+
+        self.in_directions = in_directions
+        """Count of input direction coordinates (spherical=>2, cartesian=>3)"""
+
+        self.skips = tuple(skips)
+        """Skip connection tuple for primary feature MLP"""
+
+        self.coordinate_encoder = PositionalEncoding(coord_encode_freq)
+        """Coordinate PE"""
+
+        self.direction_encoder = PositionalEncoding(dir_encode_freq)
+        """Direction PE"""
+
+        coord_dim = self.coordinate_encoder.get_out_dim(self.in_coordinates)
+        self.feature_mlp = nn.ModuleList([nn.Linear(coord_dim, hidden_size)])
+        """Primary feature MLP"""
+        # go until num_layers -1 as we already have the initial layer
+        for i in range(num_layers - 1):
+            self.feature_mlp.append(nn.Sequential(
+                # skip with +1 as we already have the initial layer
+                nn.Linear(hidden_size + (coord_dim if i+1 in self.skips else 0), hidden_size),
+                nn.ReLU(inplace=True),
+            ))
+
+        self.sigma_fc = nn.Linear(hidden_size, 1)
+        """Linear layer for sigma calculation"""
+
+        self.color_preproc = nn.Sequential(
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(inplace=True),
+        )
+        """Linear layer preprocessing features before feeding in direction"""
+
+        dir_dim = self.direction_encoder.get_out_dim(self.in_directions)
+        self.rgb_mlp = nn.Sequential(
+            nn.Linear(hidden_size + dir_dim, hidden_size // 2),
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_size // 2, 3),
+            nn.Sigmoid(),
+        )
+        """Linear layer for RGB calculation"""
+
+    def forward(self, coordinates: Tensor, directions: Tensor, skip_colors: bool = False) -> Tensor:
+        """Perform RGBS calculation
+
+        coordinates and directions must have the same dimensions for *...
+
+        Args:
+            coordinates (shape[*..., in_coordinates]): Input point coordinates
+            directions (shape[*..., in_directions]): Input directions
+            skip_colors: Skip color calculation?
+
+        Returns:
+            rgbs (shape[*..., 4]): RGB&Sigma if skip_colors=False
+            rgbs (shape[*...]): Sigma if skip_colors=True
+        """
+        coordinates = self.coordinate_encoder(coordinates)
+        features = coordinates
+        for i, fc in enumerate(self.feature_mlp):
+            if i in self.skips:
+                features = torch.cat([features, coordinates], -1)
+            features = fc(features)
+
+        sigma = self.sigma_fc(features)
+
+        if skip_colors:
+            return sigma
+
+        directions = self.direction_encoder(directions)
+        features = self.color_preproc(features)
+        features = torch.cat([features, directions], dim=-1)
+        rgb = self.rgb_mlp(features)
+
+        return torch.cat([rgb, sigma], dim=-1)
+    
---
+++ src/utils/rays.py
@@ -0,0 +1,322 @@
+import torch
+from torch import Tensor, nn
+import torch.functional as F
+from torch.utils.data import WeightedRandomSampler
+from matplotlib import pyplot as plt
+
+
+def create_rays(height: int, width: int, intrinsic: Tensor, c2w: Tensor) -> tuple[Tensor, Tensor]:
+    """Create rays cast by camera in World coordinates
+
+    Args:
+        height: Rays to take vertically (image height)
+        width: Rays to take horizontally (image width)
+        intrinsic (shape[3, 3]): Intrinsic camera matrix
+        c2w (shape[4, 4]): Extrinsic camera matrix (Camera to World)
+    
+    Returns:
+        ray_origins (shape[width, height, 3]): Ray origins in World coordinates
+        ray_directions (shape[width, height, 3]): Cartesian ray directions in World
+    """
+    focal_x = intrinsic[0, 0]
+    focal_y = intrinsic[1, 1]
+    # cx and cy handle the misalignement of the principal point with the center of the image
+    cx = intrinsic[0, 2]
+    cy = intrinsic[1, 2]
+
+    # Index each point on the image, determine ray directions to them
+    i, j = torch.meshgrid(torch.arange(width, dtype=torch.float32), torch.arange(height, dtype=torch.float32), indexing='xy')
+    directions = torch.stack((
+        (i - cx) / focal_x,
+        -(j - cy) / focal_y,
+        -torch.ones(i.shape, dtype=torch.float32)  # -1 since ray is cast away from camera
+    ), -1)
+
+    # Transform ray directions to World, origins just need to be broadcasted accordingly
+    ray_directions = F.normalize(directions @ c2w[:3, :3].T, "fro", -1)
+    ray_origins = torch.broadcast_to(c2w[:3, -1], ray_directions.shape)  # c2w last column determines position
+    
+    return ray_origins, ray_directions
+
+
+def compute_near_far_planes(c2ws: Tensor) -> tuple[float, float]:
+    """Compute minimal near and maximal far plane
+
+    Transforms the box bounded by -1 to 1 in World coordinates to camera
+    and finds the minimal near plane and maximal far plane based on distance
+
+    Args:
+        c2ws (shape[K, 4, 4]): Extrinsic camera matrices (Camera to World)
+    
+    Returns:
+        near_plane: Minimal near plane found
+        far_plane: Maximal far plane found
+    """
+    scene_bounds_min = torch.tensor([-1, -1, -1], dtype=torch.float32)
+    scene_bounds_max = torch.tensor([1, 1, 1], dtype=torch.float32)
+
+    # Transform bounding box corners to camera coordinates
+    corners = torch.tensor([
+        [scene_bounds_min[0], scene_bounds_min[1], scene_bounds_min[2]],
+        [scene_bounds_min[0], scene_bounds_min[1], scene_bounds_max[2]],
+        [scene_bounds_min[0], scene_bounds_max[1], scene_bounds_min[2]],
+        [scene_bounds_min[0], scene_bounds_max[1], scene_bounds_max[2]],
+        [scene_bounds_max[0], scene_bounds_min[1], scene_bounds_min[2]],
+        [scene_bounds_max[0], scene_bounds_min[1], scene_bounds_max[2]],
+        [scene_bounds_max[0], scene_bounds_max[1], scene_bounds_min[2]],
+        [scene_bounds_max[0], scene_bounds_max[1], scene_bounds_max[2]],
+    ])
+    
+    nears, fars = [], []
+    for c2w in c2ws:
+        corners_camera = (c2w[:3, :3] @ corners.T).T + c2w[:3, -1]
+        distances = torch.norm(corners_camera, "fro", dim=1)
+        nears.append(torch.min(distances))
+        fars.append(torch.max(distances))
+    
+    near_plane = min(distances) * 0.9  # Slightly smaller than the closest point
+    far_plane = max(distances) * 1.1  # Slightly larger than the farthest point
+
+    return near_plane.item(), far_plane.item()
+
+
+@torch.no_grad()
+def sobel_filter(images: Tensor) -> Tensor:
+    """Applies the Sobel-Feldman operator to a batch of images
+
+    Args:
+        images (shape[K, W, H, 3]): Batch of rgb images
+
+    Return:
+        edges (shape[K, W, H]): Edge intensities 
+    """
+    # Sobel-Feldman operator
+    filter = nn.Conv2d(in_channels=1, out_channels=2, kernel_size=3, stride=1, padding=1, padding_mode='zeros', bias=False, dtype=torch.float32)
+    gx = torch.tensor([
+        [3.0, 0.0, -3.0],
+        [10.0, 0.0, -10.0],
+        [3.0, 0.0, -3.0],
+    ], dtype=torch.float32)
+    gy = torch.tensor([
+        [3.0,  10.0,  3.0],
+        [0.0,  0.0,  0.0],
+        [-3.0, -10.0, -3.0],
+    ], dtype=torch.float32)
+    weights = torch.stack([gx, gy], 0).unsqueeze(1)
+    filter.weight = nn.Parameter(weights, requires_grad=False)
+
+    edges = filter(images.mean(dim=-1).unsqueeze(1))
+    edges = torch.sqrt(torch.sum(edges ** 2, dim=1))
+    return edges
+
+
+class ImportantPixelSampler(WeightedRandomSampler):
+    """Sampler implementing Importan Pixels Sampling for NeRF"""
+
+    def __init__(self, weights: Tensor, num_samples: int, replacement: bool = True, swap_strategy_iter: int = 100):
+        """Init
+
+        Args:
+            weights (shape[N]): Pixel weights assigned by edge detection
+            num_samples: Number of samples to draw per __iter__ (epoch)
+            replacement: Choose /w replacement?
+            swap_strategy_iter: Specifies at which iteration Squared Error sampling takes over fully
+        """
+        super(ImportantPixelSampler, self).__init__(weights=weights, num_samples=num_samples, replacement=replacement, generator=None)
+        weights = weights.to(torch.float32)
+        self.pixel_weights: Tensor = weights / weights.max()
+        """(shape[N]) Pixel weights assigned by edge detection"""
+
+        self.weights: Tensor = self.pixel_weights
+        """(shape[N]) Weights used for choosing the next samples"""
+
+        self.swap_strategy_iter: int = swap_strategy_iter
+        """Specifies at which iteration Squared Error sampling takes over fully"""
+
+        self.num_iters: int = 0
+        """Counts started iterations"""
+
+        self.squared_errors: Tensor = torch.ones(self.pixel_weights.shape, dtype=torch.float32)
+        """(shape[N]) Stores squared errors for pixels"""
+
+    def __iter__(self):
+        self.num_iters += 1
+        yield from super(ImportantPixelSampler, self).__iter__()
+
+    def update_errors(self, idxs: Tensor, errors: Tensor):
+        """Update squared errors and weights for given indicies
+        
+        Args:
+            idxs (shape[K]): Specifies which indicies to edit weights on
+            errors (shape[K]): Freshly calculated squared errors for the idxs
+        """
+        errors = errors.clone().cpu().detach()
+        # Epsilon evaluates to 5e-3 as the prev value contributes 20%
+        self.squared_errors[idxs] = self.squared_errors[idxs] * 0.2 + errors * 0.8  + 4e-3  # Discounted error update
+        pxw = torch.clamp(torch.tensor([1.0], dtype=torch.float32) - (self.num_iters) / self.swap_strategy_iter, 0.0, 1.0)
+        self.weights[idxs] = self.pixel_weights[idxs] * pxw + self.squared_errors[idxs] * (1 - pxw)
+
+
+def sample_ray_uniformally(origins: Tensor, directions: Tensor, near: float, far: float, num_samples: int, perturb=True) -> tuple[Tensor, Tensor, Tensor]:
+    """Uniformally sample rays and return them in the World coordinate system
+
+    Args:
+        origins (shape[N, 3]): Ray origins in World coordinates
+        directions (shape[N, 3]): Cartesian ray directions in World
+        near: Near plane, the first sample points' depth
+        far: Far plane, the last sample points' depth
+        num_samples: How many samples to take along the ray
+        perturb: If True, adds noise to the depths
+    
+    Returns:
+        points (shpe[N, num_samples, 3]): Sampled points in World coordinates
+        directions (shape[N, num_samples, 3]): Original directions expanded to match the shape of points
+        depths (shape[N, num_samples]): Depth of each sampled point on the given ray
+    """
+    device = origins.device
+    depths = torch.linspace(near, far, num_samples, dtype=torch.float32, device=device).expand(origins.shape[0], -1)
+
+    if perturb:
+        # Noise is at most half of step size, this ensures sorted depths, required for volume rendering
+        noise = (torch.rand(depths.shape, device=device) - 0.5) * (far - near) / num_samples / 2
+        # Clamping to stay between near and far
+        depths = (depths + noise).clamp(near, far)
+
+    points = origins[..., None, :] + directions[..., None, :] * depths[..., :, None]
+    # Expand directions to make NeRF input
+    directions = directions[..., None, :].expand(points.shape)
+    return points, directions, depths
+
+
+def sample_pdf(bins: Tensor, weights: Tensor, num_samples: int, deterministic: bool = False) -> Tensor:
+    """Samples based on an approximated Probability Density Function
+    
+    Args:
+        bins (shape[N, M]): Bin bounds
+        weights (shape[N, M]): Weights of bins
+        num_samples: How many samples to take
+        deterministic: If True, uses a linspace (also ensures sorted output) to re-sample instead of random
+    
+    Returns:
+        samples(shape[N, num_samples]): Set of new samples based on approximated PDF
+    """
+    device = weights.device
+
+    weights = weights + 1e-5  # avoid nans later
+    pdf = weights / torch.sum(weights, -1, keepdim=True)  # Normalize PDF
+    cdf = torch.cumsum(pdf, -1)
+    cdf = torch.cat([torch.zeros_like(cdf[..., :1], device=device), cdf], dim=-1)  # Prepend 0 to have cdf->[0,1]
+
+    if deterministic:
+        u = torch.linspace(0.0, 1.0, steps=num_samples, device=device)
+        u = u.expand(list(cdf.shape[:-1]) + [num_samples])
+    else:
+        u = torch.rand(list(cdf.shape[:-1]) + [num_samples], device=device)
+
+    # Inverting the CDF
+    u = u.contiguous()  # Need contigous memory layout for further operations
+    indexes = torch.searchsorted(cdf, u, right=True)  # Finding bins
+    # Need to ensure below and above don't leave bounds of bins
+    below = torch.max(torch.zeros_like(indexes-1), indexes-1)
+    above = torch.min((cdf.shape[-1]-1) * torch.ones_like(indexes), indexes)
+    indexes = torch.stack([below, above], dim=-1)
+
+    # Gathering sampled bins and bound probabilites
+    shape = [indexes.shape[0], indexes.shape[1], cdf.shape[-1]]
+    cdf = torch.gather(cdf.unsqueeze(1).expand(shape), dim=2, index=indexes)
+    bins = torch.gather(bins.unsqueeze(1).expand(shape), dim=2, index=indexes)
+
+    # denominator is the size of the bins
+    denominator = cdf[..., 1] - cdf[..., 0]
+    denominator = torch.where(denominator < 1e-5, torch.ones_like(denominator), denominator)
+    denominator[denominator < 1e-5] = 1.0
+    # t gives the relative position inside the bins 
+    t = (u - cdf[..., 0]) / denominator
+
+    samples = bins[..., 0] + t * (bins[..., 1] - bins[..., 0])
+    return samples
+
+
+def sample_ray_hierarchically(origins: Tensor, directions: Tensor, num_samples: int, bins: Tensor, weights: Tensor, deterministic: bool = False) -> tuple[Tensor, Tensor, Tensor]:
+    """Hierarchically sample rays and return them in the World coordinate system
+
+    Args:
+        origins (shape[N, 3]): Ray origins in World coordinates
+        directions (shape[N, 3]): Cartesian ray directions in World
+        num_samples: How many samples to take along the ray
+        bins (shape[N, M]): Bin bounds calculated from a previous sampling
+        weights (shape[N, M]): Weights of bins calculated frm a previous sampling
+        deterministic: If True, uses a linspace (also ensures sorted output) to re-sample instead of random
+    
+    Returns:
+        points (shpe[N, num_samples, 3]): Sampled points in World coordinates
+        directions (shape[N, num_samples, 3]): Original directions expanded to match the shape of points
+        depths (shape[N, num_samples]): Depth of each sampled point on the given ray
+    """
+    depths = sample_pdf(bins, weights, num_samples, deterministic=deterministic)
+
+    points = origins[..., None, :] + directions[..., None, :] * depths[..., :, None]
+    # Expand directions to make NeRF input
+    directions = directions[..., None, :].expand(points.shape)
+    return points, directions, depths
+
+
+def plot_ray_sampling(points: Tensor, origin: Tensor, cartesian_direction: Tensor, title: str):
+    """Create a 3D plot of rays from the implied camera's view and a rotated onex
+
+    Args:
+        points (shpe[N, num_samples, 3]): Sampled points in World coordinates
+        origin (shape[3]): Origin of all rays in World coordinates
+        cartesian_direction([3]): View direction for the first subplot
+        title: Title of the plot
+    """
+    points = points.cpu()
+    origin = origin.cpu()
+    cartesian_direction = cartesian_direction.cpu()
+
+    fig, axes = plt.subplots(1, 2, figsize=(16,8), subplot_kw={"projection": "3d"})
+    axes = axes.flatten()
+    fig.suptitle(title)
+    plt.tight_layout()
+    # Adding the origin so it always starts from the camera position
+    points = torch.cat([origin.expand((points.shape[0], 1, -1)), points], 1)
+
+    # Convert to spherical coordinates
+    X, Y, Z = -cartesian_direction  # Taking the negative as view_init specifies direction outward
+    R = torch.sqrt(X**2 + Y**2 + Z**2)
+    X, Y, Z = X/R, Y/R, Z/R  # normalization
+    azim = torch.rad2deg(torch.atan2(Y, X))
+    elev = torch.rad2deg(torch.arcsin(Z))
+    # Multiple angles to understand better
+    for ax, (mod_elev, mod_azim) in zip(axes, [[0,0],[-10, 60]]):
+        ax.view_init(elev + mod_elev, azim + mod_azim, 0)
+        ax.plot(points[:, :, 0], points[:, :, 1], points[:, :, 2], linewidth=0.2, markersize=2, marker='o')
+    plt.show()
+
+
+def render_rays(rgbs: Tensor, depths: Tensor) -> tuple[Tensor, Tensor]:
+    """Performs Volumetric Rendering
+
+    Args:
+        rgbs (shape[N, M, 4]): RGB and Sigma values for sampled points
+        depths (shape[N, M]): Specifies how far along the rays are the RGBSs
+
+    Returns:
+        rgb (shape[N, 3]): RGB value calculated for ray
+        depth (shape[N]): Approximated depth of ray termination
+    """
+    device = rgbs.device
+
+    distances = depths[..., 1:] - depths[..., :-1]
+    # 1e10 ensures the last color is rendered no matter what
+    distances = torch.cat([distances, torch.tensor([1e10], device=device).expand(distances[...,:1].shape)], -1)
+    # directions already normalized at ray calculation, so distances correspond to world already
+
+    alpha = 1.0 - torch.exp(-F.relu(rgbs[..., 3]) * distances)
+    # 1e10 ensures the last color is rendered no matter what
+    weights = alpha * torch.cumprod(torch.cat([torch.ones((alpha.shape[0], 1), device=device), 1. - alpha + 1e-10], -1), -1)[:, :-1]
+    rgb = torch.sum(weights[..., None] * rgbs[..., :3], dim=-2)
+    depth = torch.sum(weights * depths, dim=-1)
+
+    return rgb, depth
+
